e04989c719f5ca54b96cf604aeb9318e0e5ab72b
ArfaouiYacine <yacinearfaoui310@gmail.com>
2022-05-13 03:03:36 +0100

    generated models

---
+++ app/Models/Etudiant_langue_niveau.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Etudiant_langue_niveau extends Model
+{
+    use HasFactory;
+}
