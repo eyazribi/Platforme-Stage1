44f4564438a16adc9de0062d4678db879c041a3c
skander-houas <houasskon@gmail.com>
2022-05-28 17:57:31 +0200

    delete the start and date in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -102,8 +102,8 @@ class DatabaseSeeder extends Seeder
           'job_title' => 'senior laravel developer',
           'job_paid' => false,
           'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-          'start_date' => date('Y-m-d', mktime(0, 0, 0, 6, 1, 2022)),
-          'end_date' => date('Y-m-d', mktime(0, 0, 0, 7, 10, 2022)),
+        //  'start_date' => date('Y-m-d', mktime(0, 0, 0, 6, 1, 2022)),
+         // 'end_date' => date('Y-m-d', mktime(0, 0, 0, 7, 10, 2022)),
           'companies_id' => mt_rand(1, 20),
           'tags' => 'laravel,spring,html'
         ]
