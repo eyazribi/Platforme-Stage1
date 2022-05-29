e76aa6561e5014c609822cf3418982bf83e9741a
ArfaouiYacine <yacinearfaoui310@gmail.com>
2022-05-29 19:04:44 +0100

    work experience factory

---
+++ database/factories/Work_experienceFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+class Work_experienceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+     
+        return [
+            'job_title' => $this -> faker -> title,
+            'company' => $this -> faker -> company(),
+            'city' => $this -> faker -> city(),
+            'start_date' => $this ->faker-> date(),
+            'end_date' => $this ->faker-> date(),   
+            'description' => $this -> faker -> text, 
+            'etudiants_id' => $this -> faker -> numberBetween(0, 100),
+
+
+        ];
+    }
+}
